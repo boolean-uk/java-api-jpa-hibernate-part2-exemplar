ce9fac4ede59ffc9106c3f6452b228fdbaaa5035
Dave Ames <david.john.ames@gmail.com>
2023-06-09 10:33:39 +0100

    Implemented the Author endpoints and added a new method to the book repository to get a list of books by author id.

--- src/main/java/com/booleanuk/api/controller/AuthorController.java
+++ src/main/java/com/booleanuk/api/controller/AuthorController.java
@@ -1,11 +1,14 @@
 package com.booleanuk.api.controller;
 
 import com.booleanuk.api.model.Author;
+import com.booleanuk.api.model.Book;
 import com.booleanuk.api.repository.AuthorRepository;
+import com.booleanuk.api.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -14,9 +17,50 @@ import java.util.List;
 public class AuthorController {
     @Autowired
     private AuthorRepository authorRepository;
+    @Autowired
+    private BookRepository bookRepository;
 
     @GetMapping
     public List<Author> getAll() {
         return this.authorRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Author> getOneById(@PathVariable int id) {
+        Author author = this.authorRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No Author with that ID was found."));
+        return ResponseEntity.ok(author);
+    }
+
+    @PostMapping
+    public ResponseEntity<Author> create(@RequestBody Author author) {
+        Author createdAuthor = this.authorRepository.save(author);
+        List<Book> books = this.bookRepository.getBooksByAuthorId(createdAuthor.getId());
+        createdAuthor.setBooks(books);
+        return new ResponseEntity<Author>(createdAuthor, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Author> update(@PathVariable int id, @RequestBody Author author) {
+        Author authorToUpdate = this.authorRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No Author with that ID was found."));
+        if (author.getFirstName() == null || author.getLastName() == null || author.getEmail() == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Authors must have a first name, last name and email defined.");
+        }
+        authorToUpdate.setFirstName(author.getFirstName());
+        authorToUpdate.setLastName(author.getLastName());
+        authorToUpdate.setEmail(author.getEmail());
+        authorToUpdate.setAlive(author.isAlive());
+
+        return new ResponseEntity<Author>(this.authorRepository.save(authorToUpdate), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Author> delete(@PathVariable int id) {
+        Author authorToDelete = this.authorRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No Author with that ID was found."));
+        if (authorToDelete.getBooks().size() > 0) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Cannot delete that Author as there are books associated with them.");
+        }
+        authorToDelete.setBooks(null);
+        this.authorRepository.delete(authorToDelete);
+        return ResponseEntity.ok(authorToDelete);
     }
 }
--- src/main/java/com/booleanuk/api/repository/BookRepository.java
+++ src/main/java/com/booleanuk/api/repository/BookRepository.java
@@ -7,4 +7,5 @@ import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Integer> {
     List<Book> getBooksByPublisherId(int publisher_id);
+    List<Book> getBooksByAuthorId(int author_id);
 }
